1fcb0a1d8a17e26bf1c8a3dfb60277b914ed380a
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2021-02-08 19:19:12 -0300

    Adicionando docker cru

--- api/settings.py
+++ api/settings.py
@@ -81,10 +81,16 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+CLIENT_ENCODING = 'UTF8'
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
